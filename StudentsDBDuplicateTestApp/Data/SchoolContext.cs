f96a03e716492f714cf36cbcfcf4c771ecf92a9b
erinkayb <erinkayb@gmail.com>
2019-04-24 13:39:46 +0200

    First commit

---
+++ StudentsDBDuplicateTestApp/Data/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
